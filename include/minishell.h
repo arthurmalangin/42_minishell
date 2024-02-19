595c705bccabc47125bab06f1bd9308eea37a388
Arthur Malangin <152140753+arthurmalangin@users.noreply.github.com>
2024-02-19 17:24:01 +0000

    do test

--- include/minishell.h
+++ include/minishell.h
@@ -1,43 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amalangi <amalangin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/19 16:59:56 by amalangi          #+#    #+#             */
-/*   Updated: 2024/02/19 17:18:03 by amalangi         ###   ########.fr       */
+/*   Created: 2024/02/19 17:11:52 by amalangi          #+#    #+#             */
+/*   Updated: 2024/02/19 17:22:41 by amalangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minishell.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
 # include <limits.h>
 # include <fcntl.h>
 # include <sys/stat.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdbool.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 
-int main(int argc, char **argv, char **env)
-{
-    (void)argc;
-    (void)argv;
-    (void)env;
-    char *input;
-
-    while (1)
-    {
-        input = readline("minishell$ ");
-        if (input)
-        {
-            //add_history(input);
-            printf("%s\n", input);
-            free(input);
-        }
-    }
-    return (0);
-}
+#endif
